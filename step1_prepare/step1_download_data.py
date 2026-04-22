8bbebdf196c322258d03467cd89f95ff9e2565c1
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2026-04-22 12:14:24 +0200

    refactor: remove the `id` column
    #16

--- step1_prepare/step1_download_data.py
+++ step1_prepare/step1_download_data.py
@@ -19,10 +19,12 @@ student_ids = students_dataframe['id'].copy()
 if not student_ids.empty:
     video_learning_events_csv_url = f"https://raw.githubusercontent.com/elimu-ai/ml-datasets/main/lang-ENG/student-id-{student_ids[0]}/video-learning-events.csv"
     video_learning_events_dataframe = pandas.read_csv(video_learning_events_csv_url)
+    video_learning_events_dataframe = video_learning_events_dataframe.drop(columns=['id'])
     video_learning_events_dataframe.to_csv('step1_video_learning_events.csv', index=False)
 for student_id in student_ids[1:]:
     video_learning_events_csv_url = f"https://raw.githubusercontent.com/elimu-ai/ml-datasets/main/lang-ENG/student-id-{student_id}/video-learning-events.csv"
     print(f"URL: {video_learning_events_csv_url}")
     video_learning_events_dataframe = pandas.read_csv(video_learning_events_csv_url)
+    video_learning_events_dataframe = video_learning_events_dataframe.drop(columns=['id'])
     print(os.path.basename(__file__), 'video_learning_events_dataframe: \n{}'.format(video_learning_events_dataframe))
     video_learning_events_dataframe.to_csv('step1_video_learning_events.csv', mode='a', index=False, header=False)
